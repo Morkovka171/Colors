ceebb86a7483f512e746b000061738df2a94908b
Morkovka171 <guganowa@yandex.ru>
2022-11-02 16:59:02 +0300

    js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const cols = document.querySelectorAll('.col')
 
 document.addEventListener('keydown', (event) => {
-    if (event.code.toLowerCase() == 'space') {
+    event.preventDefault()
+    if (event.code.toLowerCase() === 'space') {
         setRandomColors()
     }
 })
@@ -9,8 +10,17 @@ document.addEventListener('keydown', (event) => {
 document.addEventListener('click', (event) => {
     const type = event.target.dataset.type
 
-    if (type == 'lock'){
-        console.log('perform lock');
+    if (type === 'lock'){
+        const node = 
+        event.target.tagName.toLowerCase() === 'i'
+        ? event.target
+        : event.target.children[0]
+
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    }
+    else if (type === 'copy') {
+        copyToClickboard(event.target.textContent)
     }
 })
 
@@ -23,11 +33,25 @@ function generateRandomColor() {
     return '#' + color
 }
 
+function copyToClickboard(text) {
+    return navigator.clipboard.writeText(text)
+}
+
 function setRandomColors() {
+    const colors = []
+
     cols.forEach(col =>{
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
         const text = col.querySelector('h2')
         const button = col.querySelector('button')
         const color = chroma.random()
+
+        if(isLocked) {
+            colors.push(text.textContent)
+            return
+        }
+
+        colors.push(color)
 
         text.textContent = color
         col.style.background = color
@@ -35,11 +59,17 @@ function setRandomColors() {
         setTextColor(text, color)
         setTextColor(button, color)
     })
+
+    updateColorsHash(colors)
 }
 
 function setTextColor(text, color) {
    const luminance = chroma(color).luminance()
    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+function updateColorsHash(colors = []) {
+    document.location.hash = colors
 }
 
 setRandomColors()
