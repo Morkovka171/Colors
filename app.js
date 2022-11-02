4636def0a0614b068cc392d4fc41cdcd863f3a84
Morkovka171 <guganowa@yandex.ru>
2022-11-02 13:38:50 +0300

    JS

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const cols = document.querySelectorAll('.col')
 
+document.addEventListener('keydown', (event) => {
+    if (event.code.toLowerCase() == 'space') {
+        setRandomColors()
+    }
+})
+
+document.addEventListener('click', (event) => {
+    const type = event.target.dataset.type
+
+    if (type == 'lock'){
+        console.log('perform lock');
+    }
+})
+
 function generateRandomColor() {
     const hexCodes = '0123456789ABCDEF'
     let color = ''
@@ -12,11 +26,20 @@ function generateRandomColor() {
 function setRandomColors() {
     cols.forEach(col =>{
         const text = col.querySelector('h2')
+        const button = col.querySelector('button')
         const color = chroma.random()
-        
+
         text.textContent = color
         col.style.background = color
+
+        setTextColor(text, color)
+        setTextColor(button, color)
     })
+}
+
+function setTextColor(text, color) {
+   const luminance = chroma(color).luminance()
+   text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
 setRandomColors()
